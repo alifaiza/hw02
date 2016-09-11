807bc858a30047152e88fc8b0459992cba083f0b
alifaiza <alifaiza@grinnell.edu>
2016-09-11 14:49:20 -0500

    Update WordGenerator.java

--- WordGenerator.java
+++ WordGenerator.java
@@ -1,3 +1,74 @@
+//package hw02;
+//import java.util.Scanner;
+//import java.io.*;
+//import java.io.File;
+//
+//
+//
+//
+//public class WordGenerator {
+//
+//	public String filename;
+//	public Scanner text = null;
+//	public int wordCounter = 0;
+//	public int sentenceCounter = 0;
+//
+//	public WordGenerator(String filename) throws IOException {
+//		this.filename = filename;
+//		this.text = new Scanner(new File(filename));
+//	}
+//
+//	public boolean hasNext() {
+//		return text.hasNext();
+//	}
+//
+//	public String next() {
+//		String nextToken = text.next();
+//		return nextToken;
+//	}
+//
+//	public int getWordCount() {
+//		
+//		while(text.hasNext()) {
+//			String nextToken = text.next();
+//			String ch = Character.toString(nextToken.charAt(nextToken.length() -1));
+//			if(ch.equals){
+//			//	break;
+//				//}
+//			wordCounter = wordCounter + 1;
+//		}
+//	}
+//	return wordCounter;
+//	}
+//
+//	public int getSentenceCount() {
+//
+//
+//		while(text.hasNext()) {
+//
+//			String nextToken = text.next();
+//			String ch = Character.toString(nextToken.charAt(nextToken.length() -1));
+//			//System.out.println(ch);
+//			if(ch.equals(".") || ch.equals("!") || ch.equals("?")) {
+//
+//				sentenceCounter = sentenceCounter + 1;
+//			}
+//		}   	
+//		return sentenceCounter;
+//	}
+//
+//
+//	public static void main(String[] args) throws IOException  {
+//		WordGenerator boom = new WordGenerator("/home/alifaiza/workspace/hw02/src/hw02/test");
+//		System.out.println(boom.getSentenceCount());
+//		System.out.println(boom.getWordCount());
+//	}
+//
+//}
+
+
+
+
 package hw02;
 import java.util.Scanner;
 import java.io.*;
@@ -8,56 +79,57 @@ import java.io.File;
 
 public class WordGenerator {
 
-    public String filename;
-    public Scanner text = null;
-    public int wordCounter = 0;
-    public int sentenceCounter = 0;
+	public String filename;
+	public Scanner text = null;
+	public int wordCounter = 0;
+	public int sentenceCounter = 0;
 
-    public WordGenerator(String filename) throws IOException {
-        this.filename = filename;
-        this.text = new Scanner(new File(filename));
-    }
+	public WordGenerator(String filename) throws IOException  {
+		this.filename = filename;
+	}
 
-    public boolean hasNext() {
-        return text.hasNext();
-    }
+	public boolean hasNext()  {
+		return text.hasNext();
+	}
 
-    public String next() {
-        String nextToken = text.next();
-        return nextToken;
-    }
+	public String next() throws IOException  {
+		this.text = new Scanner(new File(filename));
+		String nextToken = text.next();
+		text.close();
+		return nextToken;
+	}
 
-    public int getWordCount() {
-        while(text.hasNext()) {
-        	if(text.next().equals(null)){
-        		break;
-        	}
-            wordCounter += 1;
-        }
-        
-        return wordCounter;
-     }
-    
-    public int getSentenceCount() {
+	public int getWordCount() throws IOException  {
+		this.text = new Scanner(new File(filename));
+		while(text.hasNext())  {
+			if(text.next().equals(null))  {
+				break;
+			}
+			wordCounter += 1;
+		}
+		text.close();
+		return wordCounter;
+	}
 
-    	
-    	while(text.hasNext()) {
- 
-    String nextToken = text.next();
-    String ch = Character.toString(nextToken.charAt(nextToken.length() -1));
-    System.out.println(ch);
-    	if(ch.equals(".") || ch.equals("!") || ch.equals("?")) {
-   
-    		sentenceCounter = sentenceCounter + 1;
-    	}
-    	}   	
-    	return sentenceCounter;
-    }
-    	
+	public int getSentenceCount() throws IOException  {
 
-    public static void main(String[] args) throws IOException  {
-        WordGenerator boom = new WordGenerator("test");
-       System.out.println(boom.getSentenceCount());
-    }
+		this.text = new Scanner(new File(filename));
+		while(text.hasNext())  {
+			String nextToken = text.next();
+			String ch = Character.toString(nextToken.charAt(nextToken.length() -1));
+			if(ch.equals(".") || ch.equals("!") || ch.equals("?"))  {
+				sentenceCounter = sentenceCounter + 1;
+			}
+		}  
+		text.close();
+		return sentenceCounter;
+	}
+
+
+	public static void main(String[] args) throws IOException  {
+		WordGenerator boom = new WordGenerator("/home/alifaiza/workspace/hw02/src/hw02/test");
+		System.out.println(boom.getSentenceCount());
+		System.out.println(boom.getWordCount());
+	}
 
 }
